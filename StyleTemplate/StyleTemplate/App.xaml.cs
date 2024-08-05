222a3c91d178d00af6d356c079b88a8a6ebe737d
勇人 大内 <tbh.kyomei0705@gmail.com>
2024-08-05 22:26:09 +0900

    First commit

---
+++ StyleTemplate/StyleTemplate/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace StyleTemplate
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}

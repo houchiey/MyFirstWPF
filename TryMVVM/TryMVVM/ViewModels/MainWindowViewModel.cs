b683b14dd0c3c79bf09b363c789a53fdee4ebae0
勇人 大内 <tbh.kyomei0705@gmail.com>
2024-08-08 00:34:31 +0900

    MVVMサンプルアプリ作成

---
+++ TryMVVM/TryMVVM/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryMVVM.ViewModels
+{
+    public partial class MainWindowViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private int _count = 0;
+
+        [RelayCommand]
+        private void CountUp() => Count++;
+
+        [RelayCommand]
+        private void CountDown() => Count--;
+    }
+}
